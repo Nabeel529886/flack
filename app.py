f85736b7536be62b119e20d967390cab8f308498
Nabeel <na529886@gmail.com>
2020-03-21 16:50:50 +0500

    changes made

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(16)
@@ -9,6 +9,11 @@ socketio = SocketIO(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@socketio.on("msg sent")
+def recieve(msg):
+    message = msg["message"]
+    emit("msg recieve", {"message": message}, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app)
