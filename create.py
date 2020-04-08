43df55ebfbde18dc9e9fb3f44d2a8b3a2b64a302
Nabeel <na529886@gmail.com>
2020-04-08 19:04:30 +0500

    changes made

--- create.py
+++ create.py
@@ -5,7 +5,7 @@ from models import *
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
